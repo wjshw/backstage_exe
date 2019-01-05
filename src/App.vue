32bc70fb2ecbde09e3f741ba3ba3337eae2fad21
wjshw <wjshw@hotmail.com>
2019-01-05 20:27:16 +0800

    backstage powered by element-ui

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>header</el-header>
+      <el-container>
+      <el-aside>aside</el-aside>
+      <el-main>main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Axios from 'Axios'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/iconfont/iconfont.css'
+// import './assets/css/global.scss'
+
+Vue.use(Vuex)
+Vue.use(ElementUI);
+Vue.prototype.$axios = Axios
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
